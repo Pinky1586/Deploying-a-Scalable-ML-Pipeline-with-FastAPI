299aec8c5c88da4bcfeb7c9098376ae5beabd8fd
Christina Jernberg <cjernbe@wgu.edu>
2026-08-03 21:12:50 -0700

    Implement model slice testing and update gitignore

--- train_model.py
+++ train_model.py
@@ -5,6 +5,7 @@ from sklearn.model_selection import train_test_split
 import numpy as np
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import KFold, cross_val_score
+from sklearn.ensemble import RandomForestClassifier
 
 from ml.data import process_data
 from ml.model import (
@@ -15,7 +16,7 @@ from ml.model import (
     save_model,
     train_model,
 )
-# TODO: load the cencus.csv data
+#load the cencus.csv data
 project_path = "."
 data_path = os.path.join(project_path, "data", "census.csv")
 print(data_path)
@@ -71,8 +72,8 @@ X_test, y_test, encoder, lb = process_data(
     lb=lb,
 )
 
-# TODO: use the train_model function to train the model on the training dataset
-model = None # your code here
+#use the train_model function to train the model on the training dataset
+model = train_model(X_train, y_train)
 
 # save the model and the encoder
 model_path = os.path.join(project_path, "model", "model.pkl")
@@ -85,23 +86,29 @@ model = load_model(
     model_path
 ) 
 
-# TODO: use the inference function to run the model inferences on the test dataset.
-preds = None # your code here
+# use the inference function to run the model inferences on the test dataset.
+preds = inference(model, X_test)
 
 # Calculate and print the metrics
 p, r, fb = compute_model_metrics(y_test, preds)
 print(f"Precision: {p:.4f} | Recall: {r:.4f} | F1: {fb:.4f}")
 
-# TODO: compute the performance on model slices using the performance_on_categorical_slice function
+# compute the performance on model slices using the performance_on_categorical_slice function
 # iterate through the categorical features
 for col in cat_features:
     # iterate through the unique values in one categorical feature
     for slicevalue in sorted(test[col].unique()):
         count = test[test[col] == slicevalue].shape[0]
         p, r, fb = performance_on_categorical_slice(
-            # your code here
-            # use test, col and slicevalue as part of the input
+            data=test, 
+            column_nam=col, 
+            slice_value=slicevalue, 
+            categorical_features=cat_features, 
+            label="salary", 
+            encoder=encoder, 
+            lb=lb, 
+            model=model
         )
         with open("slice_output.txt", "a") as f:
             print(f"{col}: {slicevalue}, Count: {count:,}", file=f)
-            print(f"Precision: {p:.4f} | Recall: {r:.4f} | F1: {fb:.4f}", file=f)
+            print(f"Precision: {p:.4f} | Recall: {r:.4f} git status| F1: {fb:.4f}", file=f)
